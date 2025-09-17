3d153bcf525ed5280e865c7cd860452218de3780
MrTolaganov <otabektolaganov69@gmail.com>
2025-09-18 00:34:24 +0500

    ffgdfg

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-white-1 shadow-xs hover:bg-primary/90 text-base font-semibold',
+        default: 'bg-primary text-white shadow-xs hover:bg-primary/90 text-base font-semibold',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
